ba3a0cf925402df56a5cc305dfe0a451e8adae10
Lavanyamurugan2001 <114234594+Lavanyamurugan2001@users.noreply.github.com>
2022-11-01 23:05:25 +0530

    Create 5.fulladder.v

---
+++ 5.fulladder.v
@@ -0,0 +1,10 @@
+               module full_add(a,b,cin,sum,cout);
+ 	          input a,b,cin;
+  	          output sum,cout;
+                wire x,y,z;
+ 
+
+  		half_add h1(.a(a),.b(b),.s(x),.c(y));
+ 		half_add h2(.a(x),.b(cin),.s(sum),.c(z));
+ 		or o1(cout,y,z);
+endmodule
